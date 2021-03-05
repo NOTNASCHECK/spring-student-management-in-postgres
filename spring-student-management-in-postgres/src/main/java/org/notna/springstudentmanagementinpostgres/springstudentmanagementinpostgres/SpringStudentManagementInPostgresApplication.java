1891a5edae2723e2d7c351e6712a61d0a59abd09
notnaGER <75083505+notnaGER@users.noreply.github.com>
2021-03-05 10:22:15 +0100

    clean main class

--- spring-student-management-in-postgres/src/main/java/org/notna/springstudentmanagementinpostgres/springstudentmanagementinpostgres/SpringStudentManagementInPostgresApplication.java
+++ spring-student-management-in-postgres/src/main/java/org/notna/springstudentmanagementinpostgres/springstudentmanagementinpostgres/SpringStudentManagementInPostgresApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringStudentManagementInPostgresApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringStudentManagementInPostgresApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStudentManagementInPostgresApplication.class, args);
+    }
 }
