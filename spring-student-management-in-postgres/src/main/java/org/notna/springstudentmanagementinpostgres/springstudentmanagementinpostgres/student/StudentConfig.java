f0e97485377a4c9ab3564a417de9ca2a6a8941c6
notnaGER <75083505+notnaGER@users.noreply.github.com>
2021-03-05 13:58:06 +0100

    fix class StudentConfig

--- spring-student-management-in-postgres/src/main/java/org/notna/springstudentmanagementinpostgres/springstudentmanagementinpostgres/student/StudentConfig.java
+++ spring-student-management-in-postgres/src/main/java/org/notna/springstudentmanagementinpostgres/springstudentmanagementinpostgres/student/StudentConfig.java
@@ -4,17 +4,18 @@ import java.time.LocalDate;
 import java.util.Arrays;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StudentConfig {
-    private CommandLineRunner commandLineRunner(StudentRepository repository) {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
         return args->{
-                    Student mariam = new Student(1L, "Mariam", "mariam.ziff@gmail.com", LocalDate.of(2000, 1, 30), 22);
-                    Student phillip = new Student(1L, "Phillip", "phillip.ziff@gmail.com", LocalDate.of(2000, 1, 30), 22);
+                    Student mariam = new Student("Mariam", "mariam.ziff@gmail.com", LocalDate.of(2000, 1, 30), 22);
+                    Student phillip = new Student("Phillip", "phillip.ziff@gmail.com", LocalDate.of(2000, 1, 30), 22);
                     repository.saveAll(Arrays.asList(mariam, phillip));
         };
-
-
     }
 }
