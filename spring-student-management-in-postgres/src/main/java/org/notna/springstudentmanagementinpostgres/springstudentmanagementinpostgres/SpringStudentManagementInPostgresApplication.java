b2e0e05a1baf5bff755f91eeba48392487e8737f
notnaGER <75083505+notnaGER@users.noreply.github.com>
2021-03-05 09:41:56 +0100

    init spring boot projekt

---
+++ spring-student-management-in-postgres/src/main/java/org/notna/springstudentmanagementinpostgres/springstudentmanagementinpostgres/SpringStudentManagementInPostgresApplication.java
@@ -0,0 +1,13 @@
+package org.notna.springstudentmanagementinpostgres.springstudentmanagementinpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudentManagementInPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudentManagementInPostgresApplication.class, args);
+	}
+
+}
---
+++ spring-student-management-in-postgres/src/test/java/org/notna/springstudentmanagementinpostgres/springstudentmanagementinpostgres/SpringStudentManagementInPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package org.notna.springstudentmanagementinpostgres.springstudentmanagementinpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentManagementInPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
